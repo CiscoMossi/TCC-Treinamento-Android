2eb1f4171a61453e8095a8ff95f9a7d75f199c09
Gustavo Rodrigues <gustavo.rodrigues@cwi.com.br>
2018-07-28 16:07:47 -0300

    feat(splash): segura minha splash screen

--- app/src/main/java/apps/com/br/tcc/MainActivity.kt
+++ app/src/main/java/apps/com/br/tcc/MainActivity.kt
@@ -2,11 +2,17 @@ package apps.com.br.tcc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
+
+        val decorView = window.decorView
+        val uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        decorView.systemUiVisibility = uiOptions
     }
 }
